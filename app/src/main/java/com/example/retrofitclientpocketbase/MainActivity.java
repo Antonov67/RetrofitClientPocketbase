8de49a8f4c8ce8fbe441fa5943da18516c1cba9d
Dm Ant <Dm@Ant.ru>
2025-03-28 16:04:15 +0300

    отображение списка студентов на экране

--- app/src/main/java/com/example/retrofitclientpocketbase/MainActivity.java
+++ app/src/main/java/com/example/retrofitclientpocketbase/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.retrofitclientpocketbase;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.retrofitclientpocketbase.network.callbacks.SimpleDataCallback;
 import com.example.retrofitclientpocketbase.network.models.ResponseStudents;
+import com.example.retrofitclientpocketbase.network.models.Student;
 import com.example.retrofitclientpocketbase.service.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Service service;
+    private ArrayAdapter<String> adapter;
+    ListView studentsList;
+    Button addBtn, resetBtn;
+    List<String> students;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +34,30 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        resetBtn = findViewById(R.id.resetButton);
+        studentsList = findViewById(R.id.studentsList);
+
+        students = new ArrayList<>();
         service = new Service();
-        service.getAllStudents(new SimpleDataCallback<ResponseStudents>() {
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, students);
+        studentsList.setAdapter(adapter);
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onLoad(ResponseStudents data) {
-                System.out.println(data.getStudents().get(0).name);
+            public void onClick(View view) {
+                service.getAllStudents(new SimpleDataCallback<ResponseStudents>() {
+                    @Override
+                    public void onLoad(ResponseStudents data) {
+                        students.clear();
+                        for (Student student : data.getStudents()) {
+                            students.add(student.name + " " + student.age);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                });
             }
         });
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
