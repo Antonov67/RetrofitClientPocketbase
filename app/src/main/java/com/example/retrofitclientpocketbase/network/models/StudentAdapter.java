51dadc3961f800267f038a5d2e2a13bfb6d3d04b
Dm Ant <Dm@Ant.ru>
2025-04-04 15:50:50 +0300

    начало работы над метолом удаления студента

--- app/src/main/java/com/example/retrofitclientpocketbase/network/models/StudentAdapter.java
+++ app/src/main/java/com/example/retrofitclientpocketbase/network/models/StudentAdapter.java
@@ -1,12 +1,16 @@
 package com.example.retrofitclientpocketbase.network.models;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.example.retrofitclientpocketbase.R;
 
 import java.util.List;
 
@@ -19,6 +23,25 @@ public class StudentAdapter extends ArrayAdapter<Student> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.student_item, null);
+        }
+
+        TextView name = convertView.findViewById(R.id.nameText);
+        TextView age = convertView.findViewById(R.id.ageText);
+        TextView address = convertView.findViewById(R.id.cityText);
+        TextView  weight = convertView.findViewById(R.id.weightText);
+        TextView height = convertView.findViewById(R.id.heightText);
+
+        Student student = getItem(position);
+
+        name.setText(student.getName());
+        age.setText(student.getAge());
+        address.setText(student.getAddress());
+        weight.setText(student.getWeight());
+        height.setText(student.getHeight());
+
+        return convertView;
     }
 }
